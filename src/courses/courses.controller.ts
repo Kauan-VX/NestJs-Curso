57d6ea55cdfa7733ff5d424085592d35e1cbbb7b
Kauan Vieira Xavier <kauanvieiraxavierdev@gmail.com>
2024-08-29 16:54:31 -0300

    feat: feat commit

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+  @Get()
+  findAll() {
+    return 'Listagem de Cursos';
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    return `Curso com ID ${params.id}`;
+  }
+}
