29790bfec7855be27be595fc39ba9229f8ae2817
Kauan Vieira Xavier <kauanvieiraxavierk@gmail.com>
2024-10-22 22:52:44 -0300

    feat: new migration addCoursesIdToCourses

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSoureOptions } from './database.module';
 import { CreateCoursesTable1729356418968 } from 'src/migrations/1729356418968-CreateCoursesTable';
 import { CreateTagsTable1729357456988 } from 'src/migrations/1729357456988-CreateTagsTable';
 import { CreateCoursesTagsTable1729360681898 } from 'src/migrations/1729360681898-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagTable1729647601740 } from 'src/migrations/1729647601740-AddCoursesIdToCoursesTagTable';
 
 export const dataSource = new DataSource({
   ...dataSoureOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCoursesTable1729356418968,
     CreateTagsTable1729357456988,
     CreateCoursesTagsTable1729360681898,
+    AddCoursesIdToCoursesTagTable1729647601740,
   ],
 });
